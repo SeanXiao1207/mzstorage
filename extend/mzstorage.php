c31b4f6ea5a29ecce75b6896fd0e67a529e7e9e3
Sean <15814451207@139.com>
2021-08-20 17:26:01 +0800

    Update mzstorage.php
    解决ssl验证失败的问题

--- extend/mzstorage.php
+++ extend/mzstorage.php
@@ -138,6 +138,9 @@ class mzstorage
 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 信任任何证书
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); // 检查证书中是否设置域名
 
         $output = curl_exec($ch);
 
